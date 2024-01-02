a6a6536a81283b288ef4f74b30545b2444dfe1db
hewman82 <138827425+hewman82@users.noreply.github.com>
2024-01-01 18:22:46 -0800

    removeBook mutation

--- server/schemas/typeDefs.js
+++ server/schemas/typeDefs.js
@@ -43,9 +43,7 @@ const typeDefs = `
     addUser(username: String!, email: String!, password: String!): Auth
     login(email: String!, password: String!): Auth
     saveBook(bookId: ID!, input: BookInput): User
-    addComment(thoughtId: ID!, commentText: String!): Thought
-    removeThought(thoughtId: ID!): Thought
-    removeComment(thoughtId: ID!, commentId: ID!): Thought
+    removeBook(thoughtId: ID!): User
   }
 `;
 
