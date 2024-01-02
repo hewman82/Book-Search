e50502b4fa22335d93976314d60c467cf1f1501c
hewman82 <138827425+hewman82@users.noreply.github.com>
2024-01-01 18:16:11 -0800

    book input type, book type, saveBook mutation

---
+++ server/schemas/typeDefs.js
@@ -0,0 +1,52 @@
+const typeDefs = `
+  type User {
+    _id: ID
+    username: String
+    email: String
+    password: String
+    thoughts: [Thought]!
+  }
+
+  input BookInput {
+    authors: String!
+    description: String!
+    title: String!
+    bookId: String!
+    image: String! 
+    link: String
+  }
+
+  type Book {
+    _id: ID
+    authors: String
+    description: String!
+    title: String!
+    bookId: String!
+    image: String
+    link: String
+  }
+
+  type Auth {
+    token: ID!
+    user: User
+  }
+
+  type Query {
+    users: [User]
+    user(username: String!): User
+    thoughts(username: String): [Thought]
+    thought(thoughtId: ID!): Thought
+    me: User
+  }
+
+  type Mutation {
+    addUser(username: String!, email: String!, password: String!): Auth
+    login(email: String!, password: String!): Auth
+    saveBook(bookId: ID!, input: BookInput): User
+    addComment(thoughtId: ID!, commentText: String!): Thought
+    removeThought(thoughtId: ID!): Thought
+    removeComment(thoughtId: ID!, commentId: ID!): Thought
+  }
+`;
+
+module.exports = typeDefs;
